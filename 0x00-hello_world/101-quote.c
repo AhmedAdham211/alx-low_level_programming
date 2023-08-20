d30a623852d88b0405053db229ed18dc0a25fb45
AhmedAdham211 <ahmedaladham211998@gmail.com>
2023-08-19 21:15:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n", 59);
+char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, qu, 59);
 return (1);
 }
