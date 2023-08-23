9799ab8e5e446d05f21166d551cdc3188b7658fd
AhmedAdham211 <ahmedaladham211998@gmail.com>
2023-08-22 19:17:43 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+* rev_string - a function that reverse a string
+* followed by a new line
+* @s: an input string
+* Return: Nothing
+*/
+void rev_string(char *s)
+{
+int i;
+
+for (i = 0; i < strlen(s) / 2; i++)
+{
+char temp;
+
+temp = s[i];
+s[i] = s[_strlen(s) - i - 1];
+}
+}
