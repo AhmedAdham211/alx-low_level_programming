8056138fd03e14a582807eff52ccc360cc657e27
AhmedAdham211 <ahmedaladham211998@gmail.com>
2023-08-22 19:22:31 -0700

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,12 @@ void rev_string(char *s)
 {
 int i;
 
-for (i = 0; i < _strlen(s) / 2; i++)
+for (i = 0; i < strlen(s) / 2; i++)
 {
 char temp;
 
 temp = s[i];
-s[i] = s[_strlen(s) - i - 1];
-s[_strlen(s) - i - 1] = temp;
+s[i] = s[strlen(s) - i - 1];
+s[strlen(s) - i - 1] = temp;
 }
 }
