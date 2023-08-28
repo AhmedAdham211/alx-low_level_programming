05c1bbbc834413fb00020794457f50c7a358454d
AhmedAdham211 <ahmedaladham211998@gmail.com>
2023-08-28 10:48:22 -0700

    6-print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+return (0);
+}
