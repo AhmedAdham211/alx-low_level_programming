4f42217919a270cb5e09f6cc34a970dcf6a4bb8c
AhmedAdham211 <ahmedaladham211998@gmail.com>
2023-08-28 10:09:14 -0700

    4-print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,15 @@ int main(void)
 	char ch;
 
 	ch = 'a';
-	while (ch++ <= 'z')
+	while (ch <= 'z')
 		{
 		if (ch == 'q' || ch == 'e')
+			{
+			ch++;
 			continue;
+			}
 		putchar(ch);
+		ch++;
 		}
 	putchar('\n');
 	return (0);
