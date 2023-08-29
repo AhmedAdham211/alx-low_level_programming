aac6de68b7b82e603c64eb8b7a250c1e936df3e0
AhmedAdham211 <ahmedaladham211998@gmail.com>
2023-08-28 18:54:48 -0700

    str str

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <stddef.h>
 /**
  * _strstr -  a function that locates a substring.
  * @haystack: an input string to search in
@@ -10,21 +10,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *temp1 = needle, *temp2 = haystack;
-
-	while (*haystack)
+	while (*haystack != '\0')
 	{
-		temp2 = haystack;
-		needle = temp1;
-		while (*haystack == *needle)
-		{
-			haystack++;
-			needle++;
-		}
+	char *haystack_ptr = haystack;
+	char *needle_ptr = needle;
 
-		if (*needle == '\0')
-			return (haystack);
-		haystack = temp2 + 1;
+	while (*needle_ptr != '\0' && *haystack_ptr == *needle_ptr)
+	{
+	haystack_ptr++;
+	needle_ptr++;
 	}
-	return (NULL);
+	if (*needle_ptr == '\0')
+	{
+	return (haystack);
+	}
+	haystack++;
+	}
+return (NULL);
 }
